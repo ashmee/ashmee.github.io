28903be9687f24eeb2fa7cb3aafb7eaa1e31e0b4
allowemptyinput <ashmee90@gmail.com>
2022-09-03 23:16:23 +0500

    add ga

--- webpack.common.ts
+++ webpack.common.ts
@@ -3,79 +3,92 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const CopyPlugin = require("copy-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-const isProductionMode = process.env.NODE_ENV === "production"
 
-module.exports = {
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].[contenthash].css",
-            chunkFilename: "[id].[contenthash].css",
-        }),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: path.join(__dirname, "./public/index.html"),
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: path.resolve(__dirname, "public/images"),
-                    to: ".",
+module.exports = (env: any, argv: { mode: string }) => {
+    const isProductionMode = argv.mode === "production"
+
+    return {
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].[contenthash].css",
+                chunkFilename: "[id].[contenthash].css",
+            }),
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: path.join(__dirname, "./public/index.html"),
+                minify: {
+                    minifyJS: true,
+                    collapseWhitespace: true,
+                    keepClosingSlash: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    useShortDoctype: true,
                 }
-
-            ],
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: {
-                    loader: "ts-loader",
-                },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    isProductionMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            }),
+            new CopyPlugin({
+                patterns: [
                     {
-                        loader: "css-loader",
-                        options: {
-                            sourceMap: !isProductionMode,
-                        },
-                    },
-                    "postcss-loader",
-                    "sass-loader",
+                        from: path.resolve(__dirname, "public/images"),
+                        to: ".",
+                    }
+
                 ],
-            },
+            }),
         ],
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-    },
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, "./docs"),
-        clean: true,
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: "vendor",
-                    chunks: "all",
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: {
+                        loader: "ts-loader",
+                    },
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        isProductionMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: !isProductionMode,
+                            },
+                        },
+                        "postcss-loader",
+                        "sass-loader",
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        output: {
+            filename: "[name].[contenthash].js",
+            path: path.resolve(__dirname, "./docs"),
+            clean: true,
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    commons: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendor",
+                        chunks: "all",
+                    },
                 },
             },
         },
-    },
-    devtool: isProductionMode ? "" : "inline-source-map",
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "./docs"),
+        devtool: isProductionMode ? 'eval' : 'source-map' ,
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "./docs"),
+            },
+            hot: true,
+            compress: true,
+            port: 9000,
         },
-        hot: true,
-        compress: true,
-        port: 9000,
-    },
+    }
 }
